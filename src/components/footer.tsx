2343b446fbafeb3cdb0197256f7ce2d645d2a006
JuanLopez2004 <juanlopeze22004@gmail.com>
2025-07-11 00:49:59 -0500

    feat: component pages and header work

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer: React.FC = () => (
+  <footer className="w-full bg-gray-800 text-white p-4 text-center">
+    <p>&copy; {new Date().getFullYear()} My App. All rights reserved.</p>
+  </footer>
+);
+
+export default Footer;
---
+++ src/components/header.tsx
@@ -0,0 +1,54 @@
+import Link from "next/link";
+
+export default function Header() {
+    return (
+            <><style>{`
+
+              .header {
+                    display: flex;
+                    padding: 1.5rem 2.5rem;
+                    align-items: center;
+                    background-color: rgba(119,133,204);
+                    color: white;
+                    box-sizing: border-box;
+              }
+
+              .header-left {
+                    display: flex;
+                    align-items: center;
+                    gap: 1.5rem;
+              }
+
+              .header-container {
+                    width: 100%;
+                    max-width: 1500px;
+                    margin: 0 auto;
+                    display: flex;
+                    align-items: center;
+                    justify-content: space-between;
+              }
+
+              .header-logo {
+                    height: 75px;
+              }
+
+              .header-title {
+                    font-size: 2rem;
+                    font-weight: bold;
+                    letter-spacing: 1px;
+               }
+
+            `}</style>
+            <header className="header">
+              <header className="header-container">
+                <div className="header-left">
+                   <Link href="/">
+                      <img src="https://upload.wikimedia.org/wikipedia/commons/a/af/Tux.png"
+                      alt="Logo" className="header-logo" />
+                   </Link>
+                    <span className="header-title">Juan Lopez</span>
+                </div>
+              </header>
+            </header></>  
+    );
+}
